415594640e6e4e3be9c543325134523fcdd8651c
d1ezelll <diezelll@mail.ru>
2025-01-16 21:45:33 +0400

    lewis

--- js/script.js
+++ js/script.js
@@ -19,3 +19,34 @@ window.addEventListener('click', (e) => {
         header.classList.remove('open');
     }
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Создаем ссылки на элементы
+    reviewsPrevArrow = document.querySelector(".arrow-prev");
+    reviewsNextArrow = document.querySelector(".arrow-next");
+    
+    // Создаем ссылки на слайды
+    firstSlide = document.getElementById("firstItem");
+    secondSlide = document.getElementById("secondItem");
+
+    // Функция для показа следующего слайда
+    function showNextSlide() {
+        firstSlide.style.display = 'none';
+        secondSlide.style.display = 'block';
+    }
+
+    // Функция для показа предыдущего слайда
+    function showPreviousSlide() {
+        secondSlide.style.display = 'none';
+        firstSlide.style.display = 'block';
+    }
+
+    // Добавляем обработчики событий
+    reviewsPrevArrow.addEventListener('click', showPreviousSlide);
+    reviewsNextArrow.addEventListener('click', showNextSlide);
+
+    // Инициализация слайдера (показаем первый слайд)
+    showNextSlide();
+});
